084a74683dd622a32b7cdff3099f67e5dda902fa
lilmithi <gathitumithi9000@gmail.com>
2023-06-16 20:45:29 +0300

    Implement Top tab navigator

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -1,8 +1,8 @@
 import * as React from "react";
 import {
-	DarkTheme,
-	DefaultTheme,
-	NavigationContainer,
+  DarkTheme,
+  DefaultTheme,
+  NavigationContainer,
 } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Start from "../screens/Start";
@@ -18,84 +18,68 @@ import Project from "../screens/Project";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import OfflineMode from "../screens/OfflineMode";
 import { FontAwesome } from "@expo/vector-icons";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 const Stack = createNativeStackNavigator();
 export default () => {
-	const scheme = useColorScheme();
-	const uitheme = useTheme();
-	const MyTheme = {
-		...DarkTheme,
-		colors: {
-			...DarkTheme.colors,
-			background: uitheme["color-basic-800"],
-		},
-	};
+  const scheme = useColorScheme();
+  const uitheme = useTheme();
+  const MyTheme = {
+    ...DarkTheme,
+    colors: {
+      ...DarkTheme.colors,
+      background: uitheme["color-basic-800"],
+    },
+  };
 
-	const Tab = createBottomTabNavigator();
+  // const Tab = createBottomTabNavigator();
+  const Tab = createMaterialTopTabNavigator();
 
-	const BottomApp = () => (
-		<Tab.Navigator>
-			<Tab.Screen
-				name="Home"
-				component={Added_contacts}
-				options={{
-					headerTitle: "Uploaded Beneficiaries",
-					tabBarIcon: ({ color, size }) => (
-						<FontAwesome name="home" color={color} size={size} />
-					),
-				}}
-			/>
-			<Tab.Screen
-				name="Offline"
-				component={OfflineMode}
-				options={{
-					headerTitle: "Offline Beneficiaries",
-					tabBarIcon: ({ color, size }) => (
-						<FontAwesome name="power-off" color={color} size={size} />
-					),
-				}}
-			/>
-		</Tab.Navigator>
-	);
+  const BottomApp = () => (
+    <Tab.Navigator>
+      <Tab.Screen name="Uploaded" component={Added_contacts} />
+      <Tab.Screen name="Offline" component={OfflineMode} />
+    </Tab.Navigator>
+  );
 
-	return (
-		<NavigationContainer theme={scheme === "dark" ? MyTheme : DefaultTheme}>
-			<Stack.Navigator>
-				<Stack.Screen
-					name="Start"
-					options={{
-						headerShown: false,
-					}}
-					component={Start}
-				/>
-				<Stack.Screen
-					name="Login"
-					options={{
-						headerShown: false,
-					}}
-					component={Login}
-				/>
-				<Stack.Screen
-					name="AddedContacts"
-					options={{
-						headerTitle: "Beneficiaries",
-					}}
-					component={BottomApp}
-				/>
-				<Stack.Screen
-					name="Beneficiary_form"
-					options={{
-						headerTitle: "Beneficiary form",
-					}}
-					component={Project}
-				/>
-				<Stack.Screen
-					name="Benform"
-					options={{
-						headerTitle: "Beneficiary form",
-					}}
-					component={Beneficiary_form}
-				/>
-			</Stack.Navigator>
-		</NavigationContainer>
-	);
+  return (
+    <NavigationContainer theme={scheme === "dark" ? MyTheme : DefaultTheme}>
+      <Stack.Navigator initialRouteName="Start">
+        <Stack.Screen
+          name="Start"
+          options={{
+            headerShown: false,
+          }}
+          component={Start}
+        />
+        <Stack.Screen
+          name="Login"
+          options={{
+            headerShown: false,
+          }}
+          component={Login}
+        />
+        <Stack.Screen
+          name="AddedContacts"
+          options={{
+            headerTitle: "Beneficiaries",
+          }}
+          component={BottomApp}
+        />
+        <Stack.Screen
+          name="Beneficiary_form"
+          options={{
+            headerTitle: "Beneficiary form",
+          }}
+          component={Project}
+        />
+        <Stack.Screen
+          name="Benform"
+          options={{
+            headerTitle: "Beneficiary form",
+          }}
+          component={Beneficiary_form}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
